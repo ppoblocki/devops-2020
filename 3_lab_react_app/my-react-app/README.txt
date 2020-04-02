0db858babfd4d7c59f67403f49fded08cca4afa0
ppoblocki <patryk.drv@gmail.com>
2020-04-02 09:26:36 +0200

    React

---
+++ 3_lab_react_app/my-react-app/README.txt
@@ -0,0 +1,12 @@
+Lab (02.04.2020)
+
+-> Środowisko testowe a deweloperskie ?
+    -> Dockerfile (produkcyjny)
+    -> Dockerfile.dev (deweloperski)
+
+-> docker-compose
+    -> uwaga na bug w npm/react - po uruchumieniu docker-compose znika terminal - dodajemy tty: true
+
+-> Nie kopiujemy dużo do kontenera bo podpinamy dyski (volumes)
+
+-> ZADANIE: Stworzyć apkę React
---
+++ 3_lab_react_app/my-react-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! #stayhome - STOP COVID-19
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/ppoblocki"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Patryk Pobłocki
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
